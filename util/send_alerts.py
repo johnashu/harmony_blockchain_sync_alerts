48db5d8b7ad4f2fc7a642bd177f6773261db96c8
DavidWhicker <51021366+DavidWhicker@users.noreply.github.com>
2021-12-14 10:16:44 +0000

    Update send_alerts.py

--- util/send_alerts.py
+++ util/send_alerts.py
@@ -3,14 +3,12 @@ from includes.config import envs, VSTATS_API
 from util.connect import connect_to_api
 
 
-def send_to_vstats(subject: str, msg: str, alert_type: str,server: str, difference: str ) -> None:
+def send_to_vstats(subject: str, msg: str, alert_type: str) -> None:
     j = {
         "api_token": envs.VSTATS_TOKEN,
         "alert-type": alert_type,
         "subject": subject,
         "message": msg,
-        "server": msg,
-        "difference": msg,
     }
     full, _, _ = connect_to_api("", VSTATS_API, "", j=j)
     log.info(full)
@@ -34,5 +32,14 @@ def send_synced_alert(subject: str, msg: str, server: str, difference: str) -> N
     msg = f"{msg}"
     server = f"{server}"
     difference = f"{difference}"
-    send_to_vstats(subject, msg,server,difference, "info")
+
+    #send_to_vstats(subject, msg,server,difference, "info")
+    j = {
+        "api_token": envs.VSTATS_TOKEN,
+        "alert-type": alert_type,
+        "subject": subject,
+        "message": msg,
+    }
+    full, _, _ = connect_to_api("", VSTATS_API, "", j=j)
+    log.info(full)
     
