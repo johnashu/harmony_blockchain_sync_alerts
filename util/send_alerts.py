720dafda6f346ff2d8015684d553cd7bffe2bef3
johnashurst-sg <73337262+johnashurst-sg@users.noreply.github.com>
2021-12-15 11:38:30 +0100

    [UPD] alerts -> class, remove assigned envs for hot reload

--- util/send_alerts.py
+++ util/send_alerts.py
@@ -1,70 +1,66 @@
 import logging as log
-from includes.config import (
-    envs,
-    VSTATS_API,
-    hostname,
-    FULLY_SYNCED_NOTIFICATIONS,
-    FULLY_SYNCED_NOTIFICATION_LOOP_COUNT,
-    LOOP_COUNT,
-    OUR_SHARD,
-)
-
-from util.connect import connect_to_api
 
 
-def send_to_vstats(subject: str, msg: str, alert_type: str) -> None:
-    j = {
-        "api_token": envs.VSTATS_TOKEN,
-        "alert-type": alert_type,
-        "subject": subject,
-        "message": msg,
-    }
-    full, _, _ = connect_to_api("", VSTATS_API, "", j=j)
-    log.info(full)
+class Alerts:
+    def __init__(self, **kwargs) -> None:
+        self.__dict__.update(kwargs)
 
+    def send_to_vstats(self, subject: str, msg: str, alert_type: str) -> None:
+        j = {
+            "api_token": self.envs.VSTATS_TOKEN,
+            "alert-type": alert_type,
+            "subject": subject,
+            "message": msg,
+        }
+        full, _, _ = self.connect_to_api("", self.VSTATS_API, "", j=j)
+        log.info(full)
 
-def send_alert(
-    subject: str, msg: str, _type: str, log_level: log, log_msg: str
-) -> None:
-    log_level(log_msg)
-    subject = f"{subject}"
-    msg = f"{msg}"
-    send_to_vstats(subject, msg, _type)
+    def send_alert(
+        self, subject: str, msg: str, _type: str, log_level: log, log_msg: str
+    ) -> None:
+        log_level(log_msg)
+        subject = f"{subject}"
+        msg = f"{msg}"
+        self.send_to_vstats(subject, msg, _type)
 
+    def build_send_error_message(self, shard: int, *a, **kw) -> None:
+        err_msg = self.build_error_message(*a, **kw)
+        self.send_alert(
+            f"Shard {shard} Behind -- {self.hostname}",
+            err_msg,
+            "danger",
+            log.error,
+            f"Shard {shard} Behind -- {self.hostname}",
+        )
 
-def build_send_error_message(shard: int, *a, **kw) -> None:
-    err_msg = build_error_message(*a, **kw)
-    send_alert(
-        f"Shard {shard} Behind -- {hostname}",
-        err_msg,
-        "danger",
-        log.error,
-        f"Shard {shard} Behind -- {hostname}",
-    )
+    def build_error_message(
+        self,
+        local_data_shard: dict,
+        remote_data_shard: dict,
+        blocks: int,
+        _type: str = "shard",
+    ):
+        return f"<strong>Local Epoch {local_data_shard[f'{_type}-chain-header']['epoch']}:</strong> {local_data_shard[f'{_type}-chain-header']['viewID']}\n<strong>Remote Epoch {remote_data_shard['shard-chain-header']['epoch']}:</strong> {remote_data_shard['shard-chain-header']['viewID']}\n<strong>Difference:</strong> {blocks}"
 
+    def generic_error(self, e: str):
+        self.send_alert(
+            "Sync Script Error",
+            f"Alert author\n\nError Message :: {e}",
+            "danger",
+            log.error,
+            "Sending ERROR Alert..",
+        )
 
-def build_error_message(
-    local_data_shard: dict, remote_data_shard: dict, blocks: int, _type: str = "shard"
-):
-    return f"<strong>Local Epoch {local_data_shard[f'{_type}-chain-header']['epoch']}:</strong> {local_data_shard[f'{_type}-chain-header']['viewID']}\n<strong>Remote Epoch {remote_data_shard['shard-chain-header']['epoch']}:</strong> {remote_data_shard['shard-chain-header']['viewID']}\n<strong>Difference:</strong> {blocks}"
-
-
-def generic_error(e: str):
-    send_alert(
-        "Sync Script Error",
-        f"Alert author\n\nError Message :: {e}",
-        "danger",
-        log.error,
-        "Sending ERROR Alert..",
-    )
-
-
-def happy_alert(shard: int) -> None:
- 
-    send_alert(
-        f"Shard {shard} Synced -- {hostname}",
-        f"",
-        "info",
-        log.info,
-        f"Shard {shard} Synced -- {hostname}",
-    )
+    def happy_alert(self, shard: int):
+        log.info(self.LOOP_COUNT)
+        if self.FULLY_SYNCED_NOTIFICATIONS and (
+            self.LOOP_COUNT % self.FULLY_SYNCED_NOTIFICATION_LOOP_COUNT == 0
+            or self.LOOP_COUNT == 0
+        ):
+            self.send_alert(
+                f"Shard {shard} Synced -- {self.hostname}",
+                f"",
+                "info",
+                log.info,
+                f"Shard {shard} Synced -- {self.hostname}",
+            )
