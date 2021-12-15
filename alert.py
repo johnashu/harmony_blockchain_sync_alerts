f6568f0b599d3b6ca5044cd1e4e2dd429b2a8660
johnashurst-sg <73337262+johnashurst-sg@users.noreply.github.com>
2021-12-15 18:10:51 +0100

    [UPD] bettter logging for failed RPC calls

--- alert.py
+++ alert.py
@@ -19,48 +19,49 @@ def run():
                     remote_data_shard = remote_data_shard_0                    
                 else:
                     res, remote_data_shard = process_command(latest_headers(s=envs.SHARD))
-                    if not res:
-                        alerts.generic_error(remote_data_shard_0)
-                    else:  
-                        # get local server stats
-                        _, local_data_shard = process_command(latest_headers())
+                    
+                if not res:
+                    alerts.generic_error(remote_data_shard_0)
+                else:  
+                    # get local server stats
+                    _, local_data_shard = process_command(latest_headers())
 
-                        # do math to see if we're in sync - Change to int to throw error and send error alert with the message received.
-                        shard_0_blocks = int(
-                            do_maths_on_blocks(
-                                local_data_shard, remote_data_shard_0, _type="beacon"
-                            )
+                    # do math to see if we're in sync - Change to int to throw error and send error alert with the message received.
+                    shard_0_blocks = int(
+                        do_maths_on_blocks(
+                            local_data_shard, remote_data_shard_0, _type="beacon"
                         )
-                        if envs.SHARD > 0:
-                            shard_n_blocks = int(
-                                do_maths_on_blocks(local_data_shard, remote_data_shard)
-                            )
+                    )
+                    if envs.SHARD > 0:
+                        shard_n_blocks = int(
+                            do_maths_on_blocks(local_data_shard, remote_data_shard)
+                        )
 
-                        # if lower blocks on shard 0
+                    # if lower blocks on shard 0
+                    if (
+                        shard_0_blocks <= -10 or shard_0_blocks >= 10
+                    ):  # Allow 10 block swing due to API lag between calls
+                        alerts.build_send_error_message(
+                            0,
+                            local_data_shard,
+                            remote_data_shard_0,
+                            shard_0_blocks,
+                            _type="beacon",
+                        )
+                    else:
+                        alerts.happy_alert(0)
+
+                    # only if not on shard 0.
+                    if envs.SHARD > 0:
+                        # if lower blocks on shard 3
                         if (
-                            shard_0_blocks <= -10 or shard_0_blocks >= 10
+                            shard_n_blocks <= -10 or shard_n_blocks >= 10
                         ):  # Allow 10 block swing due to API lag between calls
                             alerts.build_send_error_message(
-                                0,
-                                local_data_shard,
-                                remote_data_shard_0,
-                                shard_0_blocks,
-                                _type="beacon",
+                                envs.SHARD, local_data_shard, remote_data_shard, shard_n_blocks
                             )
                         else:
-                            alerts.happy_alert(0)
-
-                        # only if not on shard 0.
-                        if envs.SHARD > 0:
-                            # if lower blocks on shard 3
-                            if (
-                                shard_n_blocks <= -10 or shard_n_blocks >= 10
-                            ):  # Allow 10 block swing due to API lag between calls
-                                alerts.build_send_error_message(
-                                    envs.SHARD, local_data_shard, remote_data_shard, shard_n_blocks
-                                )
-                            else:
-                                alerts.happy_alert(envs.SHARD)
+                            alerts.happy_alert(envs.SHARD)
 
         except Exception as e:
             alerts.generic_error(e)
