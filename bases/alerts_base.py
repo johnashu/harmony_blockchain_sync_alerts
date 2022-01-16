5390d80d3d781aa73db2e825dfc10fdb976c7320
DavidWhicker <51021366+DavidWhicker@users.noreply.github.com>
2022-01-16 10:57:11 +0000

    Update alerts_base.py

--- bases/alerts_base.py
+++ bases/alerts_base.py
@@ -1,5 +1,5 @@
 import logging as log
-
+from includes.config import hostname
 
 class AlertsBase:
     def __init__(self, VSTATS_API: str, connect_to_api: object, **kwargs) -> None:
@@ -7,12 +7,13 @@ class AlertsBase:
         self.connect_to_api = connect_to_api
         self.__dict__.update(kwargs)
 
-    def send_to_vstats(self, subject: str, msg: str, alert_type: str) -> None:
+    def send_to_vstats(self, subject: str, msg: str, alert_type: str, hostname: str = hostname) -> None:
         j = {
             "api_token": self.envs.VSTATS_TOKEN,
             "alert-type": alert_type,
             "subject": subject,
             "message": msg,
+            "hostname": hostname,
         }
         full, _, _ = self.connect_to_api("", self.VSTATS_API, "", j=j)
         log.info(full)
